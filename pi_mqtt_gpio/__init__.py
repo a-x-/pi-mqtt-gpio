2e6ceb5d7aff04d5d7e50c0d078ab761c5409502
alex mextner <Aleksandr.MEKHONOSHIN@raiffeisen.ru>
2021-02-22 19:18:20 +0300

    v0.5.7-ha-patches.4

--- pi_mqtt_gpio/__init__.py
+++ pi_mqtt_gpio/__init__.py
@@ -3,4 +3,4 @@ import yaml, os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-CONFIG_SCHEMA = read('../config.schema.yml')
+CONFIG_SCHEMA = yaml.safe_load(read('../config.schema.yml'))
