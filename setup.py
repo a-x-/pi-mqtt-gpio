011e0ffaf03c6c0ecbd31604fa1dd15fb316bbf9
alex mextner <Aleksandr.MEKHONOSHIN@raiffeisen.ru>
2021-02-22 19:01:08 +0300

    v0.5.7-ha-patches.1

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class SchemaCommand(Command):
 
 setup(
     name="pi_mqtt_gpio",
-    version="0.5.7-ha-patches",
+    version="0.5.7-ha-patches.1",
     cmdclass={"insert_schema": SchemaCommand},
     packages=find_packages(
         exclude=[
